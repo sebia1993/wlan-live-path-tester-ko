1c4af758d7587208b7dbc5b49635ae6357414492
sebia1993 | 네트워크 자동화 엔지니어 <49932643+sebia1993@users.noreply.github.com>
2026-09-04 09:20:09 +0900

    test(proxy-auth): allow friend smoke assembly

--- src/WlanLivePathTester.Core/Properties/AssemblyInfo.cs
+++ src/WlanLivePathTester.Core/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WlanLivePathTester.SelfTest")]
 [assembly: InternalsVisibleTo("WlanLivePathTester.Windows")]
+[assembly: InternalsVisibleTo("WlanLivePathTester.ProxyAuthSmoke")]
