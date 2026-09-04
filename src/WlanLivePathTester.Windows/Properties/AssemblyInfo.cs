0f7431499cb80480f040f801798dcae278a41c44
sebia1993 | 네트워크 자동화 엔지니어 <49932643+sebia1993@users.noreply.github.com>
2026-09-04 09:42:45 +0900

    test(download): add loopback measurement smoke coverage

--- src/WlanLivePathTester.Windows/Properties/AssemblyInfo.cs
+++ src/WlanLivePathTester.Windows/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WlanLivePathTester.WindowsSmoke")]
 [assembly: InternalsVisibleTo("WlanLivePathTester.ProxyAuthSmoke")]
+[assembly: InternalsVisibleTo("WlanLivePathTester.MeasurementSmoke")]
